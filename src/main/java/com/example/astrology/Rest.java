47128a0952268cea1bfd7690681d559e1986fc34
k.manasa <kalyanammanasa2004@gmail.com>
2023-11-05 09:05:34 +0530

    a12

--- src/main/java/com/example/astrology/Rest.java
+++ src/main/java/com/example/astrology/Rest.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+
 public class Rest {
 	
 	UserDAOClass udc;
